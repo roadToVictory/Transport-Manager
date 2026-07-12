5fd61ac733799e299272651b4a07d04829e019c6
roadToVictory <damian.koperstynski@gmail.com>
2026-07-12 23:54:27 +0200

    Initial commit

---
+++ src/main/java/org/example/transport/TransportApplication.java
@@ -0,0 +1,12 @@
+package org.example.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportApplication {
+    static void main(String[] args) {
+        SpringApplication.run(TransportApplication.class, args);
+    }
+
+}
